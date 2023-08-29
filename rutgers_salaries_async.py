dfe0b009fa25f8a5f93e2b98f27a0a7bda9a24e3
Ibrahim Mudassar <22484328+ibrahimmudassar@users.noreply.github.com>
2023-08-29 00:25:21 -0400

    changing csv collection

--- rutgers_salaries_async.py
+++ rutgers_salaries_async.py
@@ -1,7 +1,6 @@
 import concurrent.futures
-import csv
-from concurrent.futures import ProcessPoolExecutor
 
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
@@ -17,17 +16,6 @@ def scrub(text):
     return ''.join(x for x in text if x in string.printable)
 
 
-# open the file in the write mode
-f = open('nj_salaries.csv', 'w', newline='')
-
-# create the csv writer
-writer = csv.writer(f)
-
-# write a row to the csv file
-writer.writerow(["Name", "Campus", "Department", "Title",
-                "Hire Date", "Base Pay", "Gross Pay"])
-
-
 def person(url: str):
     response = requests.get(url).content
     soup = BeautifulSoup(response, 'html.parser')
@@ -37,31 +25,26 @@ def person(url: str):
     for table_row in table.find_all("span", {"class": "cbFormData"}):
         response.append(table_row.text)
 
-    # for table_row in table.find_all('tr')[1:]:
-    #     info = [table_h.text for table_h in table_row]
-    #     info.pop()
-    #     info[-1] = str(string_to_int(info[-1]))
-
-    #     response.append(info)
-    #     # print(info)
-
     return response
 
 
-def main():
-    url = "https://c2eku173.caspio.com/dp/9a5950006ab05c116c684a75ae49?rnd=1680724718148&RecordID=18918&cpipage=1&PageID=8&PrevPageID=8&CPISortType=&CPIorderBy=&cbCurrentPageSize=10&cbRecordPosition="
-    total = 25_553
-    urls = [url + str(i) for i in range(1, total + 1)]
+url = "https://c2eku173.caspio.com/dp/9a5950006ab05c116c684a75ae49?rnd=1680724718148&RecordID=18918&cpipage=1&PageID=8&PrevPageID=8&CPISortType=&CPIorderBy=&cbCurrentPageSize=10&cbRecordPosition="
+total = 25_553
+urls = [url + str(i) for i in range(1, total + 1)]
 
-    with ProcessPoolExecutor() as executor:
-        result = {executor.submit(person, url): url for url in urls}
+persons = []
 
-        for future in tqdm(concurrent.futures.as_completed(result), total=total):
+with concurrent.futures.ThreadPoolExecutor() as executor:
 
-            writer.writerow(future.result())
+    # Submit the tasks to the executor
+    results = [executor.submit(person, url)
+               for url in urls]
 
+    # Get the results as they complete
+    for future in tqdm(concurrent.futures.as_completed(results), total=len(results)):
+        persons.append(future.result())
 
-if __name__ == '__main__':
-    main()
-    # close the file
-    f.close()
+df = pd.DataFrame(persons, columns=["Name", "Campus", "Department", "Title",
+                                    "Hire Date", "Base Pay", "Gross Pay"])
+
+df.to_csv("rutgers_salaries.csv")
